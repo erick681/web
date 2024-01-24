4f3ab6060f8f5adfc6e55fd74fbe72c5653a4353
erick681 <erick33rick681@gmail.com>
2024-01-24 09:22:03 -0300

    adding form to template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import TarefasForm
 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+        tarefas_form(request.POST)
+        if tarefas_form.is_valid():
+            tarefas_form.save()
+    else :
+        tarefas_form = TarefasForm()
+    tarefas_form = TarefasForm()
+    return render(request,'forms_cadastro.html',{'tarefas_form': tarefas_form })
